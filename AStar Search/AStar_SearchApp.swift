c9b195a7a00ac41cf8e170f9e1544afa609a1e92
elichartnett <elichartnett@icloud.com>
2022-08-30 15:23:58 -0500

    Initial Commit

---
+++ AStar Search/AStar_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AStar_SearchApp.swift
+//  AStar Search
+//
+//  Created by Eli Hartnett on 8/30/22.
+//
+
+import SwiftUI
+
+@main
+struct AStar_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
